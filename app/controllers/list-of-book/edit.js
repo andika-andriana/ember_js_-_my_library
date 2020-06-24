1930945aa7f02df5d5fba946c21d846fc29dbffd
andika-andriana <andriana.mia1@gmail.com>
2020-06-25 03:40:24 +0700

    Adding input element

--- app/controllers/list-of-book/edit.js
+++ app/controllers/list-of-book/edit.js
@@ -1,4 +1,17 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  typeBooks: Array(),
+
+  init(){
+    this._super(...arguments);
+    let types = ["One Of Novel","Documentation","Other"];
+    this.set('typeBooks', types);
+  },
+
+  actions:{
+    onChangeCombo(item){
+      console.log(item);
+    },
+  }
 });
--- app/routes/list-of-book/edit.js
+++ app/routes/list-of-book/edit.js
@@ -1,6 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  templateName: 'list-of-book/input',
   model({id}){
     return this.store.peekRecord('book', id);
   }
